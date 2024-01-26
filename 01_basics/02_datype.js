b049a56a861b74e7d9be897c7ffcf4b12f42ff01
Nithinvaraganti <139944799+Nithinvaraganti@users.noreply.github.com>
2024-01-26 04:15:23 +0000

    datatypes

---
+++ 01_basics/02_datype.js
@@ -0,0 +1,16 @@
+let name = 'mike';
+let age = 32;
+let log = ('name');
+
+
+console.log(name);
+
+//types
+/*
+null = it is a obj'
+undefined = same
+string =" "
+boolean = T/F
+int =  range 2^53
+bigint = higher than int
+*/
